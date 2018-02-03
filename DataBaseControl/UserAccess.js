d0a7b2bb4afb36da1692f7e44cd268aeb0b51278
Ty <tycampos2001@gmail.com>
2018-02-03 01:22:00 -0500

    work on finding existing users

--- DataBaseControl/UserAccess.js
+++ DataBaseControl/UserAccess.js
@@ -1,13 +1,7 @@
-
-
-/*var exjson = {'key':'value'};
-//define key value
- exjson.key2 = {'key2':'value2'};
-//define another key value*/
 var obj = {
-   UserData:[
-   ]
-};
+   		UserData:[
+   		]
+	};
 
 	var fs = require("fs");
 	function printuserJson(){
@@ -18,20 +12,55 @@ var obj = {
 		console.log("\n *EXIT* \n");
 	}
 
-	//obj.UserData.push({username: 'nil', password: 'nil', respval: 0});
-	//var json = JSON.stringify(obj);
-	//var fs = require('fs');
-	//fs.writeFile('UserData.json', json, 'utf8');
+	
 	function newUser(user, pass){
 		fs.readFile('UserData.json', 'utf8', function readFileCallback(err, data){
    		 if (err){
    	   	  console.log(err);
    	 		 } 
    		 else {
-   		 obj = JSON.parse(data); //now it an object
-   		 obj.UserData.push({username: user, password:pass, respval: 0}); //add some data
-  		 json = JSON.stringify(obj); //convert it back to json
-  		 fs.writeFile('UserData.json', json, 'utf8'); // write it back 
+   		 	obj = JSON.parse(data); //now it an object
+   		 	obj.UserData.push({username: user, password:pass, respval: 0}); //add some data
+  		 	json = JSON.stringify(obj,0,1); //convert it back to json
+  		 	fs.writeFile('UserData.json', json, 'utf8'); // write it back 
 			}
-	});
-}
+		});
+	}
+	function findUser(user){
+		content = fs.readFile('UserData.json', 'utf8', function readFileCallback(err, data){
+   		 if (err){
+   	   	  console.log(err);
+   	 		 } 
+   		 else {
+   		 	obj = JSON.parse(data); //now it an object
+   		 	for(var key in obj){
+   		 		console.log(obj.username);
+   		 		a = obj[key];
+   		 		for(var obja in key){
+   		 			console.log(key)
+
+   		 		}
+   		 	}
+			}
+		});
+
+	}
+
+findUser("asd")
+	
+
+
+
+
+
+
+
+/*var exjson = {'key':'value'};
+//define key value
+exjson.key2 = {'key2':'value2'};
+//define another key value*/
+//obj.UserData.push({username: 'nil', password: 'nil', respval: 0});
+//var json = JSON.stringify(obj);
+//var fs = require('fs');
+//fs.writeFile('UserData.json', json, 'utf8');
+
